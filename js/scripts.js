9cfdd20476258903e500f50809ff44ce706f74bf
Eliaslichi96 <146300075+Eliaslichi96@users.noreply.github.com>
2023-10-09 11:49:27 -0400

    added pokemon list array
    added 8 pokemons to the list, including name, height and type/.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-alert('Hello world');
-let favoriteFood = "tacos"
-document.write(favoriteFood)
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+    {name: 'Charmander', height: 0.6, type:['fire']},
+    {name: 'Squirtle', height:0.5, type:['water']},
+    {name: 'Pikachu', height:0.4, type:['electric']},
+    {name: 'Abra', height:0.9, type:['psychic']},
+    {name: 'Scyther', height:1.5 , type:['bug','flying']},
+    {name: 'Moltres', height:2, type:['fire','flying']},
+    {name: 'Lugia', height:5.2 , type:['psychic','flying']},
+]

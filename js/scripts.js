ee0bf10e1a039e33f9ae440f01faa0fa9ec77f8c
Eliaslichi96 <146300075+Eliaslichi96@users.noreply.github.com>
2023-10-23 20:30:26 -0400

    1.5 IIFE changes
    added repository, return

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-let pokemonList = [
+let pokemonRepository = (function(){
+  let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
     {name: 'Charmander', height: 0.6, type:['fire']},
     {name: 'Squirtle', height:0.5, type:['water']},
@@ -7,9 +8,20 @@ let pokemonList = [
     {name: 'Scyther', height:1.5 , type:['bug','flying']},
     {name: 'Moltres', height:2, type:['fire','flying']},
     {name: 'Lugia', height:5.2 , type:['psychic','flying']},
-]
+];
 
-pokemonList.forEach (function(pokemon) {
-  document.write('<p>' +'This is' + " " + pokemon.name + ',' + " " + 'the height is:'+ " " + pokemon.height + 
-    ' type: ' + pokemon.type + '.' +'</p>');
-});
+return {
+  getAll: function(){
+    return pokemonList;
+  },
+  add: function (item) {
+      pokemonList.push(item);
+}
+} 
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  if (pokemon.height > 0) {
+    document.write('<p>' +'This is' + " " + pokemon.name + ',' + " " + 'the height is:'+ " " + pokemon.height + 
+    ' type: ' + pokemon.type + '.' +'</p>')}});
+  
